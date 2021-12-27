ef24f378dfbbe91416c103a37255b95f03c0c38a
naschemaa <96695646+naschemaa@users.noreply.github.com>
2021-12-27 19:02:06 +0100

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,47 @@
+module.exports = {
+    name: 'ping',
+    description: 'Ping.',
+    execute(message) {
+      message.channel.send('Pong.');
+    }
+  };
+
+  module.exports = {
+    name: 'Porn',
+    description: 'Nothing XD',
+    execute(message) {
+      message.channel.send('YEAHH I WANT WATCH PORN TOO');
+    }
+  };
+
+  module.exports = {
+    name: 'SCP',
+    description: 'SCP1',
+    execute(message) {
+      message.channel.send('What about SCP?');
+    }
+  };
+
+  module.exports = {
+    name: 'lil nas x',
+    description: 'LNX',
+    execute(message) {
+      message.channel.send('Lil Nas X is my boyfreind lol');
+    }
+  };
+
+  module.exports = {
+    name: 'naschemaa',
+    description: 'naschemaa',
+    execute(message) {
+      message.channel.send('naschemaa is the besttttttt!');
+    }
+  };
+  
+  module.exports = {
+    name: 'gate B',
+    description: 'GB',
+    execute(message) {
+      message.channel.send('SCP-106 has broken out of the facility!');
+    }
+  };
